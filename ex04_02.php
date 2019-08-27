3aa0bb1ec96eb883431611ec79c688a1e7b5157c
hitourebito <yuto0423777@gmail.com>
2019-08-27 12:12:20 +0900

    numeric value check

--- ex04_02.php
+++ ex04_02.php
@@ -4,13 +4,17 @@
     if($_POST["name"] == NULL) {
       echo "名前が入力されていません", "<br/>";
     }
+
     if($_POST["age"] == NULL) {
       echo "年齢が入力されていません", "<br/>";
     }
 
-    if (is_numeric($_POST["age"]) == FALSE) {
+    if (is_numeric($_POST["age"]) === FALSE) {
       echo "年齢が数値ではありません";
     }
 
+    if ($_POST["name"] == TRUE && $_POST["age"] == TRUE && is_numeric($_POST["age"]) == TRUE) {
+      echo $_POST["age"], "歳の", $_POST["name"], "さん、いらっしゃい";
+    }
   ?>
 </body>
