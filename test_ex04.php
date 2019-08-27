dd12025716cc0d6b82cefa37cda79f843cdfcc96
hitourebito <yuto0423777@gmail.com>
2019-08-27 11:03:46 +0900

    formタグとtextareaタグを追加

--- test_ex04.php
+++ test_ex04.php
@@ -3,6 +3,10 @@
   <head>
   </head>
   <body>
-    
+    <form action="test_ex04.php" method="POST">
+      <textarea name="jusyo" cols="50" rows="3">初期値</textarea><br/>
+      <input type="submit" name="btn" value="送信"><br/>
+    </form>
+      入力したデータは<?php echo $_POST["jusyo"];?>です。
   </body>
 </html>
