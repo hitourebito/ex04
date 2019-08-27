55fe9973b0f65403f351871af49fddddd576c815
hitourebito <yuto0423777@gmail.com>
2019-08-27 11:10:52 +0900

    input type=passwordに変更

--- test_ex04.php
+++ test_ex04.php
@@ -4,9 +4,9 @@
   </head>
   <body>
     <form action="test_ex04.php" method="POST">
-      <input type="hidden" name="data" value="himitsu"><br/>
+      <input type="password" name="pw" value=""><br/>
       <input type="submit" name="btn" value="送信"><br/>
     </form>
-      入力したデータは<?php echo $_POST["jusyo"];?>です。
+      入力したデータは<?php echo $_POST["pw"];?>です。
   </body>
 </html>
