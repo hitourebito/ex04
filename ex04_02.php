89920e2a2eb9e606f1ac0f9a027370c03774f143
hitourebito <yuto0423777@gmail.com>
2019-08-27 12:29:43 +0900

    rewrite ex04_02.php

--- ex04_02.php
+++ ex04_02.php
@@ -1,19 +1,24 @@
 <body>
   <?php 
+    $errflg = 0;
 
-    if($_POST["name"] == NULL) {
+    if($_POST["name"] == "") {
       echo "名前が入力されていません", "<br/>";
+      $errflg += 1;
     }
 
-    if($_POST["age"] == NULL) {
+    if($_POST["age"] == "") {
       echo "年齢が入力されていません", "<br/>";
+      $errflg += 1;
+
     }
 
-    if (is_numeric($_POST["age"]) === FALSE) {
+    if (is_numeric($_POST["age"]) == FALSE) {
       echo "年齢が数値ではありません";
+      $errflg += 1;
     }
 
-    if ($_POST["name"] == TRUE && $_POST["age"] == TRUE && is_numeric($_POST["age"]) == TRUE) {
+    if ($errflg === 0) {
       echo $_POST["age"], "歳の", $_POST["name"], "さん、いらっしゃい";
     }
   ?>
