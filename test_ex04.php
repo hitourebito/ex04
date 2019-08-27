6c8cb756750afc2805ef2b19241dd985679159a4
hitourebito <yuto0423777@gmail.com>
2019-08-27 11:08:29 +0900

    input type=hiddenに変更

--- test_ex04.php
+++ test_ex04.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <form action="test_ex04.php" method="POST">
-      <textarea name="jusyo" cols="50" rows="3">初期値</textarea><br/>
+      <input type="hidden" name="data" value="himitsu"><br/>
       <input type="submit" name="btn" value="送信"><br/>
     </form>
       入力したデータは<?php echo $_POST["jusyo"];?>です。
