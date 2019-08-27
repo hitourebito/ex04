fa960a8080e740ce44b4dbfac67c21df0405fdb0
hitourebito <yuto0423777@gmail.com>
2019-08-27 11:15:50 +0900

    fix input type="radio"

--- test_ex04.php
+++ test_ex04.php
@@ -4,9 +4,13 @@
   </head>
   <body>
     <form action="test_ex04.php" method="POST">
-      <input type="password" name="pw" value=""><br/>
+      <input type="radio" name="age" value="20歳未満" checked>20歳未満<br/>
+      <input type="radio" name="age" value="20歳代" >20歳代<br/>
+      <input type="radio" name="age" value="30歳代" >30歳代<br/>
+      <input type="radio" name="age" value="40歳以上" >40歳以上<br/>
+
       <input type="submit" name="btn" value="送信"><br/>
     </form>
-      入力したデータは<?php echo $_POST["pw"];?>です。
+      入力したデータは<?php echo $_POST["age"];?>です。
   </body>
 </html>
