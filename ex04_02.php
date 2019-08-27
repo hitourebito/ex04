688f1ce0a8222fa405f800096074c76d4b8ad819
hitourebito <yuto0423777@gmail.com>
2019-08-27 12:03:19 +0900

    enter value check

--- ex04_02.php
+++ ex04_02.php
@@ -1,3 +1,16 @@
 <body>
-  <?php ?>
+  <?php 
+
+    if($_POST["name"] == NULL) {
+      echo "名前が入力されていません", "<br/>";
+    }
+    if($_POST["age"] == NULL) {
+      echo "年齢が入力されていません", "<br/>";
+    }
+
+    if (is_numeric($_POST["age"]) == FALSE) {
+      echo "年齢が数値ではありません";
+    }
+
+  ?>
 </body>
