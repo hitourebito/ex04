03dc0fa4c061abf95652881ddbfbc51274668819
hitourebito <yuto0423777@gmail.com>
2019-08-29 00:08:29 +0900

    rewrite ex04_04.php

--- ex04_04.php
+++ ex04_04.php
@@ -30,11 +30,21 @@
           break;
       }
     }
-    
+  ?>
+
+<form action="ex04_04.php" method="POST">
+  <input type="text" name="number1" value="<?php echo $num1 ?>">
+  <input type="text" name="enzansi" value="<?php echo $enzan ?>">
+  <input type="text" name="number2" value="<?php echo $num2 ?>">
+    <?php if($errflg === 0) {
+      echo "=", $sum;
+    } ?><br/>
+  <input type="submit" name="btn" value="送信">
+</form>
+
+  <?php
     if($errflg != 0) {
       echo "正しく入力してください";
-    } else {
-      echo $num1, $enzan, $num2, "=", $sum;
-    }
+    } 
   ?>
 </body>
